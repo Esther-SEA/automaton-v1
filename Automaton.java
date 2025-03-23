11dd798246c8cf1a81ba139d368732bc3103af89
Esther <sohesther.assoa@gmail.com>
2025-03-23 13:25:30 -0400

    24-34

--- Automaton.java
+++ Automaton.java
@@ -20,7 +20,8 @@ public class Automaton
     public Automaton(int numberOfCells)
     {
         this.numberOfCells = numberOfCells;
-        state = new int[numberOfCells];
+        state = new int[numberOfCells + 1];// Extend array by one element
+        state[numberOfCells] = 0; // Last element is always 0
         // Seed the automaton with a single 'on' cell in the middle.
         state[numberOfCells / 2] = 1;
         //Adding other cells in the 1 state
@@ -47,30 +48,31 @@ public class Automaton
     /**
      * Update the automaton to its next state.
      */
-    public void update()
+    /**public void update()
     {
         // Build the new state in a separate array.
         int[] nextState = new int[state.length];
         // Naively update the state of each cell
         // based on the state of its two neighbors.
         for(int i = 0; i < state.length; i++) {
-            int left, center, right;
-            if(i == 0) {
-                left = 0;
-            }
-            else {
-                left = state[i - 1];
-            }
-            center = state[i];
-            if(i + 1 < state.length) {
-                right = state[i + 1];
-            }
-            else {
-                right = 0;
-            }
+            int left = (i == 0)? 0: state[i - 1];
+            int center = state[i];
+            int right = (i + 1 < state.length) ? state[i + 1] : 0;
+            
             nextState[i] = (left + center + right) % 2;
         }
-        state = nextState;
+         state = nextState;
+    }*/
+    public void update(){
+        int[] nextState = new int[state.length];
+        int left = 0;
+        int center = state[0];
+            for (int i=0; i < numberOfCells; i++){
+                 int right = state[i + 1];
+                 nextState[i] = calculateNextState(left, center, right);
+                 left = center;
+                 center = right;
+            }
     }
     
     /**
@@ -82,5 +84,9 @@ public class Automaton
         // Seed the automaton with a single 'on' cell.
         state[numberOfCells / 2] = 1;
             
+    }
+    
+    public int calculateNextState(int left, int center, int right){
+        return (left + center + right) % 2; 
     }
 }
--- AutomatonController.java
+++ AutomatonController.java
@@ -61,6 +61,8 @@ public class AutomatonController
      * 25. yes the same patterns emerges.
      * 26. 2, they set all elements of an array to the same value/ in reset method it is used by setting all emelents of the state array by 0. which resets the automaton
      * 27. still the same pattern of triangles repeats, it is deterministic because it always results in the same pattern.
-     * 
+     * 29. Yes there is a difference the pattern is now only a diagonal line. With nextState, it looked at the original state of all cells before making changes. but now it uses the new updated values.
+     * 30. another solution could be to use variables to store the current cell's vallue and the next cell's value before updating the cell. Then when it is updated, move the stored values forward for the next one.
+     * 33. the number of posibilities isnt infinite its 256 because you have three inputs and since they are binary, there are 2 possibilities for each input, 2^3 = 8. and the next state for each combination can also be 0 or 1 so two choice. 2^8= 256 wich means there are 256 different sets of rules for calculating the next state of a cell.
      */
 }
